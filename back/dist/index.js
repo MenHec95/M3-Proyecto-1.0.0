031d918b3085a3b6e7c0269a1c7819668ba41a1b
MenHec95 <hectormendez_95@yahoo.com>
2025-06-25 23:45:17 -0300

    L2 1.0.0

---
+++ back/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const a = 5;
+const b = 3;
+const suma = (a, b) => {
+    return a + b;
+};
+console.log(suma(a, b));
---
+++ back/src/index.ts
@@ -0,0 +1,9 @@
+const a:number =5;
+const b:number =3;
+
+const suma = (a:number, b:number):number=>{
+
+return a+b;
+}
+
+console.log (suma(a,b))
