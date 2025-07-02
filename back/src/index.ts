4a06b85f2db98580cd7247ab4bfedb7620f15c7c
MenHec95 <hectormendez_95@yahoo.com>
2025-07-01 23:37:00 -0300

    eslint

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,9 +1,9 @@
-const a:number =5;
-const b:number =3;
+const a: number = 5;
+const b: number = 3;
+const c = "4";
 
-const suma = (a:number, b:number):number=>{
+const suma = (a: number, b: number): number => {};
 
-return a+b;
-}
+console.log(suma(a, b));
 
-console.log (suma(a,b))
+const string = "hola";
