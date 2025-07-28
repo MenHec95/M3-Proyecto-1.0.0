ce68fd8ad24dc1788387abbbbac7a07d437e70ad
MenHec95 <hectormendez_95@yahoo.com>
2025-07-28 20:12:05 -0300

    Estilos Visuales 1.0.2

--- front/vite-project/src/components/Navbar/Navbar.jsx
+++ front/vite-project/src/components/Navbar/Navbar.jsx
@@ -3,22 +3,28 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <>
-      <nav>
-        <div>
-          <img src="/img/logo.png" />
-          <span>LUNAS ESPACIO HOLISTICO & SPA</span>
-        </div>
+    <nav className="navbar-container">
+      <div className="navbar-brand">
+        <img
+          src="/img/logo.png" // Asume que logo.png está en public/img
+          alt="Lunas Espacio Holístico"
+          className="navbar-logo"
+        />
+        <h1 className="navbar-title">
+          LUNAS
+          <br />
+          ESPACIO HOLÍSTICO & SPA
+        </h1>
+      </div>
 
-        <ul>
-          <li>Inicio</li>
-          <li>Mis turnos</li>
-          <li>
-            <Usuario />
-          </li>
-        </ul>
-      </nav>
-    </>
+      <ul className="navbar-links">
+        <li className="nav-item">Inicio</li>
+        <li className="nav-item">Mis Turnos</li>
+        <li className="nav-item">
+          <Usuario />
+        </li>
+      </ul>
+    </nav>
   );
 };
 
--- front/vite-project/src/components/usuario/Usuario.jsx
+++ front/vite-project/src/components/usuario/Usuario.jsx
@@ -1,13 +1,12 @@
+import "./Usuario.css";
 const user = "Usuario";
 
 const Usuario = () => {
   return (
-    <>
-      <div>
-        <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/astral-7998996-6580607.png?f=webp" />
-        <span>{user}</span>
-      </div>
-    </>
+    <div className="usuario">
+      <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/astral-7998996-6580607.png?f=webp" className="usuarioAvatar" />
+      <span className="usuarioName">{user}</span>
+    </div>
   );
 };
 
