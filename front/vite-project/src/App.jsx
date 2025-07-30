618781ed8afd70e3d49648cf2faf3b786b546d98
MenHec95 <hectormendez_95@yahoo.com>
2025-07-29 23:55:28 -0300

    Register css 1.0.1

--- front/vite-project/src/App.jsx
+++ front/vite-project/src/App.jsx
@@ -7,7 +7,7 @@ import Register from "./views/Register/Register";
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
       {/* <MisTurnos /> */}
       <Register />
       {/* <Login /> */}
--- front/vite-project/src/views/Register/Register.jsx
+++ front/vite-project/src/views/Register/Register.jsx
@@ -1,17 +1,17 @@
 import { Formik, Form, Field } from "formik";
-
+import "./Register.css";
 export default function Register() {
   return (
     <Formik initialValues={{ name: "", email: "", birthdate: "", nDni: "", username: "", password: "" }}>
-      <Form>
+      <Form className="register-form">
         <label>Nombre y Apellido</label>
-        <Field type="text" name="name" />
+        <Field type="text" name="name" placeholder="Nombres y Apellidos" />
         <label>Email</label>
-        <Field type="text" name="email" />
+        <Field type="text" name="email" placeholder="usuario@proveedor.com" />
         <label>Fecha de nacimiento</label>
-        <Field type="text" name="birthdate" />
-        <label>Numero de documento</label>
-        <Field type="number" name="nDni" />
+        <Field type="text" name="birthdate" placeholder="12-10-1995" />
+        <label>Número de documento</label>
+        <Field type="number" name="nDni" placeholder="1111111" />
         <label>Usuario</label>
         <Field type="text" name="username" />
         <label>Contraseña</label>
