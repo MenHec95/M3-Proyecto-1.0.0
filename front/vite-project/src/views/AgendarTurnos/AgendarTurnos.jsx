207675e340024cbaac28224f7d1f0f916f2bb15d
MenHec95 <hectormendez_95@yahoo.com>
2025-08-06 00:30:50 -0300

    agendar 2.0

--- front/vite-project/src/views/AgendarTurnos/AgendarTurnos.jsx
+++ front/vite-project/src/views/AgendarTurnos/AgendarTurnos.jsx
@@ -14,44 +14,44 @@ export default function AgendarTurnos() {
     },
     validate: agendarValidates,
     onSubmit: (values) => {
-      //   axios
-      //     .post("", values)
-      //     .then((res) => {
-      //       if (res.status === 201) {
-      //         Swal.fire({
-      //           title: "¡Turno agendado!",
-      //           icon: "success",
-      //           text: "Has agendado tu turno correctamente",
-      //           confirmButtonColor: "#9A86A4",
-      //         });
-      //         navigate("/MisTurnos");
-      //       }
-      //       console.log(res);
-      //     })
-      //     .catch((error) => {
-      // console.log(error);
-      //   if (error.response.data.message.includes("email")) {
-      //     Swal.fire({
-      //       title: "Error",
-      //       text: `Ocurrió un problema al registrar, ya existe un Usuario con este el email ${formik.values.email}`,
-      //       icon: "error",
-      //       confirmButtonColor: "#e74c3c",
-      //     });
-      //   }
-      // });
+      axios
+        .post("", values)
+        .then((res) => {
+          if (res.status === 201) {
+            Swal.fire({
+              title: "¡Turno agendado!",
+              icon: "success",
+              text: "Has agendado tu turno correctamente",
+              confirmButtonColor: "#9A86A4",
+            });
+            navigate("/MisTurnos");
+          }
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.response.data.message.includes("email")) {
+            Swal.fire({
+              title: "Error",
+              text: `Ocurrió un problema al registrar, ya existe un Usuario con este el email ${formik.values.email}`,
+              icon: "error",
+              confirmButtonColor: "#e74c3c",
+            });
+          }
+        });
       console.log("Enviando datos:", values);
     },
   });
 
   return (
-    <div className="register-section">
-      <div className="register-overlay">
-        <h2 className="register-title">
+    <div className="agendar-section">
+      <div className="agendar-overlay">
+        <h2 className="agendar-title">
           LUNAS
           <br />
           Agendar Turno
         </h2>
-        <form className="register-form" onSubmit={formik.handleSubmit}>
+        <form className="agendar-form" onSubmit={formik.handleSubmit}>
           <label>Fecha del turno</label>
           <input type="date" name="date" placeholder="Fecha del turno" onChange={formik.handleChange} value={formik.values.date} />
           {formik.errors.date ? formik.errors.date : ""}
